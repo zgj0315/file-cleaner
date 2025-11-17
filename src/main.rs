f98a01612154c1c9466dcda76dade2ba66480e36
赵光建 <zhaoguangjian@cyberkl.com>
2025-11-17 21:55:44 +0800

    rust works

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use walkdir::WalkDir;
+
 slint::include_modules!();
 
 fn main() -> anyhow::Result<()> {
@@ -18,6 +20,81 @@ fn main() -> anyhow::Result<()> {
             ui.set_action_text("Scan".into());
         }
     });
+
+    let ui_weak = ui.as_weak();
+    ui.on_action_clicked(move || {
+        let ui = ui_weak.unwrap();
+
+        let current_action = ui.get_action_text().to_string();
+        let dir = ui.get_selected_directory().to_string();
+
+        if dir.is_empty() {
+            eprintln!("未选择目录");
+            return;
+        }
+
+        let patterns = collect_patterns(&ui);
+
+        if current_action == "Scan" {
+            println!("开始扫描: {dir}");
+            let found_files = scan_files(&dir, &patterns);
+            println!("扫描发现 {} 个垃圾文件", found_files.len());
+
+            ui.set_action_text("Clean".into());
+        } else {
+            println!("开始清理: {dir}");
+            let found_files = scan_files(&dir, &patterns);
+
+            for f in found_files {
+                println!("delete {f}");
+                // let _ = std::fs::remove_file(&f);
+            }
+            println!("清理完成");
+
+            ui.set_action_text("Scan".into());
+        }
+    });
     ui.run()?;
     Ok(())
+}
+
+fn collect_patterns(ui: &AppWindow) -> Vec<&'static str> {
+    let mut p = vec![];
+    if ui.get_org_enabled() {
+        p.push("*.org~");
+    }
+    if ui.get_html_enabled() {
+        p.push("*.html~");
+    }
+    if ui.get_dsstore_enabled() {
+        p.push("DS_Store");
+    }
+    p
+}
+
+fn scan_files(dir: &str, patterns: &[&str]) -> Vec<String> {
+    let mut found = vec![];
+
+    for entry in WalkDir::new(dir) {
+        if let Ok(entry) = entry {
+            if entry.file_type().is_file() {
+                let name = entry.file_name().to_string_lossy().to_string();
+                for p in patterns {
+                    if pattern_match(&name, p) {
+                        found.push(entry.path().to_string_lossy().into_owned());
+                    }
+                }
+            }
+        }
+    }
+    found
+}
+
+fn pattern_match(file: &str, pat: &str) -> bool {
+    if pat.starts_with("*.") {
+        let ext = &pat[2..];
+        return file.ends_with(ext);
+    } else {
+        return file.contains(pat);
+    }
 }
